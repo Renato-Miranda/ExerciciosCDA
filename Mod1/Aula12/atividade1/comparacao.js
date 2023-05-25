cb017ef71231226ea5597c35214d241cdaa3218f
Renato <renatomna@gmail.com>
2023-05-25 17:30:58 -0300

    realizado exercicio 7

---
+++ Mod1/Aula12/atividade1/comparacao.js
@@ -0,0 +1,17 @@
+var alimento = prompt('Qual o alimento?')
+var valorSemanaPassada = prompt('Qual valor foi pago na semana passada?')
+var valorSemanaAtual = prompt('qual o valor atual?')
+
+
+
+if (alimento == 'tomate' || alimento == 'laranja' || alimento == 'abacaxi'|| alimento == 'limão'){
+    var valorSemanaPassada = Number(prompt('Qual valor foi pago na semana passada?'))
+    var valorSemanaAtual = Number(prompt('qual o valor atual?'))
+    
+    if(valorSemanaPassada > valorSemanaAtual){
+        console.log('O alimento: ' + alimento + 'diminuiu em $' + 5)
+    }
+
+}else {
+    console.log(Alimento + ' indisponivel na feira')
+}
---
+++ Mod1/Exercicios obrigatorios/ex7/script.js
@@ -0,0 +1,7 @@
+var numeroInteiro = Number(prompt('Digite um numero inreiro'))
+
+if (numeroInteiro >= 10){
+    console.log('"O número digitado é maior ou igual a 10')
+}else {
+    console.log('O número digitado é menor que 10')
+}
