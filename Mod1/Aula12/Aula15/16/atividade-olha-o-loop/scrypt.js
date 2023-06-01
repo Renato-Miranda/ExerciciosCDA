09c1770971aaa68ee5405b5f2899877e1f5c74ec
Renato <renatomna@gmail.com>
2023-06-01 10:00:37 -0300

    realizado primeira atividade

---
+++ Mod1/Aula12/Aula15/16/atividade-olha-o-loop/scrypt.js
@@ -0,0 +1,15 @@
+var nPositivo = parseInt(prompt('Digite um numero'))
+
+function listaDecrescente (){
+    while (nPositivo > 0 ){
+        nPositivo = nPositivo -1
+        console.log('\n' + nPositivo )
+    }
+}
+
+listaDecrescente()
+
+ver numero = 0
+function fizz (){
+
+}
