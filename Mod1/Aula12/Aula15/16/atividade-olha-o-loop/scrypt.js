7c059ef60eccfc1cd4008b96eabfa8f518d26eea
Renato <renatomna@gmail.com>
2023-06-01 12:06:37 -0300

    resolvido exercicio 2

--- Mod1/Aula12/Aula15/16/atividade-olha-o-loop/scrypt.js
+++ Mod1/Aula12/Aula15/16/atividade-olha-o-loop/scrypt.js
@@ -1,15 +1,46 @@
-var nPositivo = parseInt(prompt('Digite um numero'))
+// var nPositivo = parseInt(prompt('Digite um numero'))
 
-function listaDecrescente (){
-    while (nPositivo > 0 ){
-        nPositivo = nPositivo -1
-        console.log('\n' + nPositivo )
+// function listaDecrescente (){
+//     while (nPositivo > 0 ){
+//         nPositivo = nPositivo -1
+//         console.log('\n' + nPositivo )
+//     }
+// }
+
+// listaDecrescente()
+
+
+// function fizz (numero){
+//     var controle = 0
+//     var resposta = ' '
+//     while(controle <= numero){
+//         if (controle % 3 === 0) {
+//             resposta = resposta + '\nfizz'
+//         }else{
+//             resposta = resposta + '\n${controle}'
+//         }
+//         controle++
+//     }
+//     console.log(resposta)
+
+// }
+
+// fizz(10)
+
+function fizz(numero) {
+    var controle = 0;
+    var resposta = '';
+  
+    while (controle <= numero) {
+      if (controle % 3 === 0) {
+        resposta = resposta + `\nfizz`;
+      } else {
+        resposta = resposta + `\n${controle}`
+      }
+      controle++;
     }
-}
-
-listaDecrescente()
-
-var numero = 0
-function fizz (){
-
-}
+  
+    console.log(resposta);
+  }
+  
+  fizz(10);
