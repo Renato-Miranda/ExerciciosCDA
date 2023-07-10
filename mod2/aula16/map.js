3c6a689cd124f08c9367fb861ba739c9937d6d44
Renato <renatomna@gmail.com>
2023-07-10 11:57:34 -0300

    feat: aula16

---
+++ mod2/aula16/map.js
@@ -0,0 +1,23 @@
+const alunos = {
+    victroMathes : 2000,
+    AnaCarol: 3500,
+    Joao: 2500
+} 
+
+const valorAumento = {
+    victroMathes : 0,
+    AnaCarol: 0,
+    Joao: 0
+} 
+
+// Object.values(alunos) // pega só os valores
+
+
+const arrayDeAumentos = Object.entries(alunos).map((elemento) =>{
+    const response = [elemento[0 ], 4000 - elemento[1]]
+    valorAumento[response[0]] = response[1]
+    return response
+})
+
+console.log(arrayDeAumentos);
+console.log(valorAumento);
