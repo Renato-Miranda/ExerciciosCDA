f14caab4a2ccedca7b159a40c0a399cb80012b94
Renato <renatomna@gmail.com>
2023-07-18 18:21:28 -0300

    feat:exercicio personagem morreu

---
+++ Mod3/script.js
@@ -0,0 +1,10 @@
+function personagemMorreu(dano, saude) {
+    const novasaude = saude - dano
+        if (novasaude < 0){
+       console.log(1)
+    }else{
+        console.log(0);
+    }
+    
+    }
+    personagemMorreu(11,10)
