e7956356fe18880e0d7d882571297e9b235415cd
Renato <renatomna@gmail.com>
2023-08-21 19:55:13 -0300

    feat: exercicio promessas

--- Mod4/aula01/hello_world.js
+++ Mod4/aula01/hello_world.js
@@ -16,20 +16,36 @@
 
 
 
-const promessa = new Promise((resolve, reject) => {
-    let hoje = 'segunda'
-    if (hoje === 'segunda'){
-        resolve(console.log('Segundou'));
+// const promessa = new Promise((resolve, reject) => {
+//     let hoje = 'segunda'
+//     if (hoje === 'segunda'){
+//         resolve(console.log('Segundou'));
+//     }else {
+//         reject(console.log('hoje é sabado-feira'))
+//     }
+// })
+
+// promessa.then((res) => {
+//     console.log(res)
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+
+
+const chamada = 'enviada'
+
+const comunicacaoPerdida = new Promise ((resolve, reject) => {
+    if(chamada === 'enviada'){
+        resolve('OK, todos vivos na estação')
     }else {
-        reject(console.log('hoje é sabado-feira'))
+        reject('Comunicação perdida')
     }
 })
 
-promessa.then((res) => {
-    console.log(res)
-}).catch((err) => {
-    console.log(err);
+comunicacaoPerdida.then((res)=>{
+    console.log('Sucesso: ' + res);
 })
-
-
-
+.catch((err) =>{
+    console.log('Falha: ' + err);
+})
