39ab7e49716446ac3b4695f6b559f2f1a45fc882
Renato <renatomna@gmail.com>
2023-08-28 22:17:17 -0300

    feat: teste em forma de classe

--- Mod4/teste.js
+++ Mod4/teste.js
@@ -1,31 +1,71 @@
-import readline from 'readline';
+import { log } from 'console';
+import readline  from 'readline';
+import { createInterface } from 'readline';
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const propriedades = [];
 
-const propriedades = [];
+// function obterEntrada() {
+//   rl.question('Digite uma propriedade de CSS (ou "SAIR" para encerrar): ', entrada => {
+//     if (entrada.toUpperCase() === 'SAIR') {
+//       rl.close();
+//     } else {
+//       propriedades.push(entrada);
+//       obterEntrada();
+//     }
+//   });
+// }
 
-function obterEntrada() {
-  rl.question('Digite uma propriedade de CSS (ou "SAIR" para encerrar): ', entrada => {
-    if (entrada.toUpperCase() === 'SAIR') {
-      rl.close();
-    } else {
-      propriedades.push(entrada);
-      obterEntrada();
-    }
-  });
+// rl.on('close', () => {
+//   const propriedadesOrdenadas = propriedades.sort();
+//   console.log('\nPropriedades ordenadas de A-Z:');
+//   propriedadesOrdenadas.forEach(propriedade => {
+//     console.log(propriedade);
+//   });
+//   process.exit(0);
+// });
+
+// obterEntrada();
+
+class AnimalList {
+  constructor(){
+    this.animals = [],
+    this.rl = createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  }
+
+  start(){
+    console.log("Digite nome de animais ou digite 'SAIR' para encerrar");
+    this.rl.on('line', input => {
+      if (input.toUpperCase() === 'SAIR') {
+        this.rl.close();
+      } else {
+        this.addAnimal(input.toUpperCase());
+      }
+    })
+    this.rl.on('close', ()=> {
+      this.showSortedAnimals()
+      console.log("Programa encerrado");
+    })
+  }
+
+  addAnimal(animal){
+     this.animals.push(animal)
+  }
+
+  showSortedAnimals(){
+    const sortedAnimals = this.animals.sort()
+    console.log("Animais ordenados ");
+    sortedAnimals.forEach(animal => console.log(animal))
+  }
+
+
 }
 
-rl.on('close', () => {
-  const propriedadesOrdenadas = propriedades.sort();
-  console.log('\nPropriedades ordenadas de A-Z:');
-  propriedadesOrdenadas.forEach(propriedade => {
-    console.log(propriedade);
-  });
-  process.exit(0);
-});
-
-obterEntrada();
-
+const animalList = new AnimalList()
+animalList.start()
